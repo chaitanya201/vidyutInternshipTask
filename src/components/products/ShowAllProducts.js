1a3edb258ba39f107834a5c31b15cfec4601bee8
Chaitanya Sawant <chaitanyasawant05072001@gmail.com>
2022-07-13 02:14:02 +0530

    initial work

---
+++ src/components/products/ShowAllProducts.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+export default function ShowAllProducts() {
+  const [products, setProducts] = useState(null);
+  const [alertMsg, setAlertMsg] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getProducts = async () => {
+    document.title = "All Products";
+    try {
+      const response = await axios.get("https://fakestoreapi.com/products");
+      setProducts(response.data);
+    } catch (error) {
+      setAlertMsg("Server Error. Try again later.");
+    }
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
+  return (
+    <div>
+      <div>{alertMsg && <div className="text-danger">{alertMsg} </div>}</div>
+      <div className="m-4">
+        {isLoading && (
+          <div className="d-flex justify-content-center ">
+            <div className="spinner-border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        )}
+      </div>
+      <div>
+        {!isLoading && products && products.length > 0 && (
+          <div className="row m-2">
+            <div className="display-3 d-flex justify-content-center mb-2 ">
+              All Products
+            </div>
+            {products.map((product) => {
+              // Setting rating
+              let rating = "⭐";
+              if (Math.floor(product.rating.rate) === 5) rating = "⭐⭐⭐⭐⭐";
+              else if (Math.floor(product.rating.rate) === 4)
+                rating = "⭐⭐⭐⭐";
+              else if (Math.floor(product.rating.rate) === 3) rating = "⭐⭐⭐";
+              else if (Math.floor(product.rating.rate) === 2) rating = "⭐⭐";
+              else rating = "⭐";
+              return (
+                <div key={product.id} className="col p-3">
+                  <div
+                    className="card"
+                    style={{ width: "18rem", height: "30rem" }}
+                  >
+                    <div
+                      className="d-flex justify-content-center align-items-center"
+                      style={{ width: "15rem" }}
+                    >
+                      <img
+                        src={product.image}
+                        className="card-img-top w-50 "
+                        alt="..."
+                      />
+                    </div>
+                    <div className="card-body ">
+                      <div className="d-flex justify-content-center align-items-center">
+                        <h5 className="card-title">{product.title} </h5>
+                      </div>
+                      <p className="card-text">
+                        {product.description.slice(0, 20)}...{" "}
+                      </p>
+                      <div>
+                        <div className="h4">${product.price}</div>
+                        <div>{rating}, {product.rating.count}</div>
+                        <div className="d-flex justify-content-center p-2">
+                          <Link
+                            className="btn btn-primary"
+                            to={"/show-single-product"}
+                            state={{ product: product }}
+                          >
+                            View
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/products/ShowSingleProduct.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+export default function ShowSingleProduct() {
+  const location = useLocation();
+
+  try {
+    // extracting product from the link
+    var { product } = location.state;
+  } catch (error) {
+    // If user directly visits this page then showing this message.
+    return (
+      <div className="text-danger">
+        VISIT THIS PAGE THROUGH PROPER STEPS ONLY....
+      </div>
+    );
+  }
+
+  // Setting document title to title of the product.
+  document.title = product.title;
+
+  // Setting rating
+  let rating = "⭐";
+  if (Math.floor(product.rating.rate) === 5) rating = "⭐⭐⭐⭐⭐";
+  else if (Math.floor(product.rating.rate) === 4) rating = "⭐⭐⭐⭐";
+  else if (Math.floor(product.rating.rate) === 3) rating = "⭐⭐⭐";
+  else if (Math.floor(product.rating.rate) === 2) rating = "⭐⭐";
+  else rating = "⭐";
+
+  return (
+    <div className="m-5">
+    <div className="m-2">
+        <Link to={'/'} className='bg-info text-white p-2 rounded '>BACK</Link>
+    </div>
+      <div className="d-flex me-5 pe-5">
+        <div className="d-flex justify-content-center border border-2 flex-fill">
+          <img src={product.image} alt="product-img" className="w-50" />
+        </div>
+        <div className="border border-2 flex-fill">
+          <div className="m-4">
+            <div className="d-flex justify-content-center h2 p-4">
+              {product.title}
+            </div>
+            <div
+              style={{ paddingRight: "15%" }}
+              className="d-flex justify-content-between"
+            >
+              <h5>About this item </h5>
+              <text className="bg-primary text-white p-2 rounded">
+                {product.category}
+              </text>
+            </div>
+            <div>{product.description}</div>
+            <div className="h4 mt-3 me-3">Price: ${product.price}</div>
+
+            <div style={{ paddingRight: "15%" }} className="d-flex">
+              <div className="h5">Ratings:</div>
+              <div>
+                {rating} {product.rating.count}
+              </div>
+            </div>
+            <div>
+              <div className="center">
+                <div className="buttons d-flex flex-row">
+                  <div className="cart">
+                    <i className="fa fa-shopping-cart"></i>
+                  </div>
+                  <button className="btn btn-success cart-button px-5">
+                    <span className="dot"></span>
+                    Buy Now
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="center pt-2">
+              <div className="buttons d-flex flex-row">
+                <div className="cart">
+                  <i className="fa fa-shopping-cart"></i>
+                </div>
+                <button className="btn btn-info cart-button px-5">
+                  <span className="dot"></span>
+                  Add To Cart
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
